3ba6e4fe6971ca28402da17cc10364a54de17b58
Lachlan Grose <lachlan.grose@monash.edu>
2021-03-12 21:18:27 +1100

    updating conda build

--- setup.py
+++ setup.py
@@ -16,12 +16,25 @@ from pathlib import Path
 import platform
 import shutil
 from os.path import join
+import codecs
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
    
 
 setup(
     name="surfepy",
-    version=0.3,
+    version=get_version("surfepy/__init__.py"),
     author="Michael Hillier",
     description="python bindings for surfe - geological interpolator using rbf",
     long_description="",
@@ -29,4 +42,5 @@ setup(
     cmake_args=['-DEIGEN3_INCLUDE_DIR=eigen-git-mirror'],
     packages=['surfepy'],
     cmake_install_dir="surfepy"
+    # extra_objects=['']
     )
