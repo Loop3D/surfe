f0399cebefca5a17085fb8b7e3e134a26a179e40
Lachlan Grose <lachlan.grose@gmail.com>
2021-09-28 14:27:23 +1000

    fix: ld path changes

--- surfepy/__init__.py
+++ surfepy/__init__.py
@@ -1,6 +1,9 @@
 import os
 import pathlib
 # add surfe package to path for C++ libs
-os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + [pathlib.Path(__file__).parent.resolve()]
+if 'LD_LIBRARY_PATH' in os.environ:
+    os.environ['LD_LIBRARY_PATH'].append(pathlib.Path(__file__).parent.resolve()) 
+else:
+    os.environ['LD_LIBRARY_PATH'] = [pathlib.Path(__file__).parent.resolve()]
 
 from ._surfepy import *
