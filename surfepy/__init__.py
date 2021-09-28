da33443cb68a68ad29793dc28799624bfdb9aa9a
Lachlan Grose <lachlan.grose@gmail.com>
2021-09-28 14:37:54 +1000

    fix: more library path testing

--- surfepy/__init__.py
+++ surfepy/__init__.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 # add surfe package to path for C++ libs
 if 'LD_LIBRARY_PATH' in os.environ:
-    os.environ['LD_LIBRARY_PATH']+':{}'.format(pathlib.Path(__file__).parent.resolve())
+    os.environ['LD_LIBRARY_PATH']=os.environ['LD_LIBRARY_PATH']+':{}'.format(pathlib.Path(__file__).parent.resolve())
 else:
     os.environ['LD_LIBRARY_PATH'] = pathlib.Path(__file__).parent.resolve()
 
