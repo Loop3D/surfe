44149b42bdadb73ff87988db262035ff06217959
Lachlan Grose <lachlan.grose@gmail.com>
2021-10-01 10:51:09 +1000

    removed extra objects

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys
 from setuptools.command.install import install
-# from setuptools.command.bdist_wheel import bdist_wheel
+# from setuptools.command import bdist
 
 from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
@@ -13,7 +13,15 @@ except ImportError:
     print("Install it running: python -m pip install scikit-build")
     sys.exit(1)
 
-  
+# class CustomInstallLibCommand(bdist):
+#     def run(self):
+#         bdist_wheel.run(self)
+#         if os.path.isdir(self.build_dir):
+#             shutil.copyfile(self.build_dir+'/surfepy/libmath_lib.so','/usr/lib/libmath_lib.so')
+#             shutil.copyfile(self.build_dir+'/surfepy/libsurfe_lib.so','/usr/lib//libsurfe_lib.so')
+
+#             # shutil.copyfile(join(self.build_dir,Path('/surfepy/libmath_lib.{}'.format(ext)),self.install_dir+'../../libmath_lib.{}'.format(ext))
+            # shutil.copyfile(self.build_dir+'/surfepy/libsurfe_lib.{}'.format(ext),self.install_dir+'../../libsurfe_lib.{}'.format(ext))     
 
 setup(
     name="surfepy",
@@ -26,5 +34,5 @@ setup(
                '-DCMAKE_INSTALL_RPATH="$ORIGIN/../lib:$ORIGIN/../..'],
     packages=['surfepy'],
     cmake_install_dir="surfepy",
-    extra_objects=['libmath_lib','libsurfe_lib']
+    # extra_objects=['libmath_lib','libsurfe_lib']
     )
