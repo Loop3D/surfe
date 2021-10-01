6f868da338e7c2ba7276c370500a947e3b88d697
Lachlan Grose <lachlan.grose@gmail.com>
2021-10-01 14:40:55 +1000

    custom install lib

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import shutil
 from setuptools.command.install import install
 # from setuptools.command import bdist
 
@@ -12,17 +14,18 @@ except ImportError:
     print("scikit-build is required to build from source!", file=sys.stderr)
     print("Install it running: python -m pip install scikit-build")
     sys.exit(1)
-
-# class CustomInstallLibCommand(bdist):
-#     def run(self):
-#         bdist_wheel.run(self)
-#         if os.path.isdir(self.build_dir):
-#             shutil.copyfile(self.build_dir+'/surfepy/libmath_lib.so','/usr/lib/libmath_lib.so')
-#             shutil.copyfile(self.build_dir+'/surfepy/libsurfe_lib.so','/usr/lib//libsurfe_lib.so')
-
-#             # shutil.copyfile(join(self.build_dir,Path('/surfepy/libmath_lib.{}'.format(ext)),self.install_dir+'../../libmath_lib.{}'.format(ext))
+from skbuild.command.install_lib import install_lib
+class CustomInstallLibCommand(install_lib):
+    def run(self,*args,**kwargs):
+        super().run()
+        if os.path.isdir(self.install_dir):
+            
+            shutil.copyfile(self.install_dir+'/surfepy/libmath_lib.so','/tmp/libmath_lib.so')
+            shutil.copyfile(self.install_dir+'/surfepy/libsurfe_lib.so','/tmp/libsurfe_lib.so')
+            
+            # shutil.copyfile(join(self.build_dir,Path('/surfepy/libmath_lib.{}'.format(ext)),self.install_dir+'../../libmath_lib.{}'.format(ext))
             # shutil.copyfile(self.build_dir+'/surfepy/libsurfe_lib.{}'.format(ext),self.install_dir+'../../libsurfe_lib.{}'.format(ext))     
-
+        
 setup(
     name="surfepy",
     version=0.51,
@@ -30,6 +33,7 @@ setup(
     description="python bindings for surfe - geological interpolator using rbf",
     long_description="",
     license="MIT",
+    cmdclass={'install_lib': CustomInstallLibCommand},
     cmake_args=['-DEIGEN3_INCLUDE_DIR=eigen-git-mirror',
                '-DCMAKE_INSTALL_RPATH="$ORIGIN/../lib:$ORIGIN/../..'],
     packages=['surfepy'],
