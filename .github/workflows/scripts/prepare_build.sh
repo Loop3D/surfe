2dd03edde4e173bc30e70c39ef03b1a8b6440f2b
Lachlan Grose <lachlan.grose@monash.edu>
2024-10-04 16:27:29 +1000

    ci: name of scripts directory + moving python reqs to pyproject.toml

--- .github/workflows/scripts/prepare_build.sh
+++ .github/workflows/scripts/prepare_build.sh
@@ -6,4 +6,3 @@ sudo apt-get install -y build-essential cmake git libeigen3-dev pybind11-dev
 
 echo "Installation of pybind11, Eigen, and CMake is complete."
 
-pip install pybind11 oldest-supported-numpy cython scikit-build-core pybind11
